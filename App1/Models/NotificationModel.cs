b7dc219f5c7769a405aba297ecf29ffa2c7e622f
Doryan LOPEZ <lopezdoryan@gmail.com>
2020-11-16 09:20:23 +0100

    First commit

---
+++ App1/Models/NotificationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+
+namespace App1.Models
+{
+    public class NotificationModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public bool IsRead { get; set; }
+
+        public ObservableCollection<NotificationModel> fromJson(string json)
+        {
+            ObservableCollection<NotificationModel> test = JsonConvert.DeserializeObject<ObservableCollection<NotificationModel>>(json);
+            return test;
+        }
+
+    }
+}
---
+++ App1/ViewModels/HomeViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using App1.Models;
+
+using Xamarin.Forms;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace App1.ViewModels
+{
+    public class HomeViewModel : ContentPage, INotifyPropertyChanged
+    {
+        private NotificationModel _notificationModel = new NotificationModel();
+        private ObservableCollection<NotificationModel> _notifications = new ObservableCollection<NotificationModel>();
+        public ObservableCollection<NotificationModel> Notifications
+        {
+            get { return _notifications; }
+            set
+            {
+                if (_notifications != value)
+                {
+                    _notifications = value;
+                }
+            }
+        }
+
+
+        private string notifs = "[{\"title\":\"Notification 1\",\"content\":\"Lorem Ipsum\",\"publishedAt\":\"" + DateTime.Now.ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'") + "\",\"isRead\":false},{\"title\":\"Notification 2\",\"content\":\"Lorem Ipsum\",\"publishedAt\":\"" + DateTime.Now.ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'") + "\",\"isRead\":false},{\"title\":\"Notification 3\",\"content\":\"Lorem Ipsum\",\"publishedAt\":\"" + DateTime.Now.ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'") + "\",\"isRead\":false}]";
+
+
+
+
+        public HomeViewModel()
+        {
+            _notifications = _notificationModel.fromJson(notifs);
+        }
+    }
+}
